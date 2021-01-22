55a4ea8ec66d86766ea6cecb57f03c40df310923
Chris Coltsman <chris.coltsman@googlemail.com>
2021-01-22 12:47:15 +0100

    Initial project

---
+++ src/test/java/io/ccoltx/spring/boot/kafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ccoltx.spring.boot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
